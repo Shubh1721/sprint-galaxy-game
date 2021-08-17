169e13b1b6677f80f28cd9f956a5a63eed950253
Shubham Tiwari <65031177+Shubh1721@users.noreply.github.com>
2021-08-17 22:21:01 +0530

    Draw multiple vertical lines with fixed spacing
    Draw multiple vertical lines with fixed spacing and arrange the lines according to screen size.

--- galaxy.py
+++ galaxy.py
@@ -6,7 +6,11 @@ from kivy.properties import NumericProperty
 class MainWidget(Widget):
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
-    line=None
+    # line=None
+    V_NB_LINES = 7
+    V_LINES_SPACING = .1 # percenntage in screen width
+    vertical_lines = []
+
     def __init__(self, **kwargs):
         super(MainWidget,self).__init__(**kwargs)
         print("INIT W:"+str(self.width)+" H:"+str(self.height))
@@ -33,18 +37,20 @@ class MainWidget(Widget):
     def init_vertical_lines(self):
         with self.canvas:
             Color(1,1,1)
-            self.line = Line(points=[100,0,100,100])
+            # self.line = Line(points=[100,0,100,100])
+            for i in range(0, self.V_NB_LINES):
+                self.vertical_lines.append(Line())
 
     def update_vertical_lines(self):
-        center_x = int(self.width / 2)
-        self.line.points = [center_x, 0, center_x, 100]
+        center_line_x = int(self.width / 2)
+        # self.line.points = [center_x, 0, center_x, 100]
 
-
-
-
-
-        
-
+        offset = -int(self.V_NB_LINES/2)
+        spacing = self.V_LINES_SPACING*self.width 
+        for i in range(0, self.V_NB_LINES):
+            line_x = int(center_line_x + offset*spacing)
+            self.vertical_lines[i].points = [line_x, 0, line_x, self.height]
+            offset += 1
 
 
 class GalaxyApp(App):
