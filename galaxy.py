99c833981967b8c2bb6ec900217266d14f52bb2f
Shubham Tiwari <65031177+Shubh1721@users.noreply.github.com>
2021-08-17 22:02:20 +0530

    Draw vertical lines
    Imported color and line to draw vertical lines in center of screen and  update line according to screen size.

--- galaxy.py
+++ galaxy.py
@@ -1,5 +1,7 @@
 from kivy.app import App 
 from kivy.uix.widget import Widget
+from kivy.graphics.context_instructions import Color
+from kivy.graphics.vertex_instructions import Line
 from kivy.properties import NumericProperty
 class MainWidget(Widget):
     perspective_point_x = NumericProperty(0)
@@ -13,15 +15,39 @@ class MainWidget(Widget):
         pass 
     # to get window size dynamically
     def on_size(self, *args):
-        print("Size W:"+str(self.width)+" H:"+str(self.height))
+        # print("Size W:"+str(self.width)+" H:"+str(self.height))
         # self.perspective_point_x = self.width/2 # we can use similar in .kv file
         # self.perspective_point_y = self.height*0.75
-        pass 
+        self.update_vertical_lines()
+        # pass 
 
     def on_perspective_point_x(self, widget, value):
-        print("PX :"+str(value))
+        # print("PX :"+str(value))
         pass
     def on_perspective_point_y(self, widget, value):
-        print("PY:"+str(value))
-        
+        # print("PY:"+str(value))
         pass
+# # Now we are going to create vertical lines
+# that adapt accordingn to screen
+
+    def init_vertical_lines(self):
+        with self.canvas:
+            Color(1,1,1)
+            self.line = Line(points=[100,0,100,100])
+
+    def update_vertical_lines(self):
+        center_x = int(self.width / 2)
+        self.line.points = [center_x, 0, center_x, 100]
+
+
+
+
+
+        
+
+
+
+class GalaxyApp(App):
+    pass
+
+GalaxyApp().run()
